d162b70e9ecf68918541e7bfacf13ce8537507cf
mac Ray <rztechdude@yahoo.com>
2022-05-25 16:52:39 -0400

    conditionally render and testing

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
-  const [isModalOpen, setIsModalOpen]= useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -122,16 +124,16 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter(photo => photo.category === category);
 
-  const [currentPhoto, setCurrentPhoto] = useState();
-
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
-  }
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
-
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
